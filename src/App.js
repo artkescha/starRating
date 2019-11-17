5c8630f683394a333fd8f385dbb2feeb0d7b04cb
Artem Savarin <asavarin1910@gmail.com>
2019-11-17 22:11:11 +0300

    transition to ES7

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import StarRatingNoState from './StarRange_no_state';
+import StarRating from './StarRange';
 
 class App extends Component {
-    constructor(props) {
-        super()
-        this.state = { starsSelected: 0 }
-        this.change = this.change.bind(this)
-    }
-    change(starsSelected) {
-        this.setState({ starsSelected: starsSelected })
-    }
-    
+
+    state = {
+    starsSelected: 1,
+  };
+
+change=(starsSelected) => 
+       this.setState({ starsSelected: starsSelected });
 
   render() {
     return (
       <div className="App">
-      <StarRatingNoState starsSelected={this.state.starsSelected} totalStars={5} onRate={this.change} />
+      <StarRating starsSelected={this.state.starsSelected} totalStars={5} onRate={this.change} />
       </div>
     );
   }
--- src/StarRange.js
+++ src/StarRange.js
@@ -2,15 +2,15 @@ import React from 'react';
 import Star from './Star';
 
 
-const StarRatingNoState = ({starsSelected, totalStars=5, onRate=f=>f}) =>
-   <div className="star-rating">
+const StarRating = ({starsSelected, totalStars=5, onRate=f=>f}) =>
+<div className="star-rating">    
             {[...Array(totalStars)].map((n, i) =>
             <Star key={i}
              selected={i<starsSelected}
              onClick={() => onRate(i+1)}/>
              )}
-<p>{starsSelected} of {totalStars} stars</p>
+<div className="raiting"> {starsSelected} of {totalStars} stars</div>
 </div>
 
 
-export default StarRatingNoState
+export default StarRating
