267d6f10d5758a9af79f2322f8db1f0ae8e7eaf0
Artem Savarin <asavarin1910@gmail.com>
2019-11-19 04:30:00 +0300

    a little refactoring

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ class App extends Component {
 
     state = {
     starsSelected: 1,
-  };
+  }
 
 change=(starsSelected) => 
-       this.setState({ starsSelected: starsSelected });
+       this.setState({starsSelected});
 
   render() {
+  	const starsSelected = this.state.starsSelected 
     return (
       <div className="App">
-      <StarRating starsSelected={this.state.starsSelected} totalStars={5} onRate={this.change} />
+      <StarRating starsSelected={starsSelected} totalStars={5} onRate={this.change} />
       </div>
     );
   }
